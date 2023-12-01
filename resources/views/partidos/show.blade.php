51b623c9abf75ae50878a4709881a8497d98bde3
titicuevas <enrique.cuevas@iesdonana.org>
2023-12-01 23:51:33 +0100

    Boton Borrar partido solo aparece para el usuario que crea el partido

--- resources/views/partidos/show.blade.php
+++ resources/views/partidos/show.blade.php
@@ -74,52 +74,57 @@
                 </form>
 
 
-{{-- Boton Borrarse del partido el creador --}}
+                {{-- Boton Borrarse del partido el creador --}}
+                <!-- Agrega un botón solo si el usuario actual es el creador del partido -->
+                @if (auth()->user()->id === $partido->user_id)
+                    <!-- Agrega un botón para eliminar el partido con una función de confirmación -->
+                    <form method="POST" action="{{ route('partidos.destroy', $partido) }}" x-data="{ showModal: false }">
+                        @csrf
+                        @method('DELETE')
 
-<!-- Agrega un botón para eliminar el partido con una función de confirmación -->
-<form method="POST" action="{{ route('partidos.destroy', $partido) }}" x-data="{ showModal: false }">
-    @csrf
-    @method('DELETE')
+                        <!-- Botón para abrir la ventana modal -->
+                        <button type="button" x-on:click="showModal = true"
+                            class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline-red">
+                            Eliminar partido
+                        </button>
 
-    <!-- Botón para abrir la ventana modal -->
-    <button type="button" x-on:click="showModal = true"
-        class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline-red">
-        Eliminar partido
-    </button>
+                        <!-- Ventana modal de confirmación -->
+                        <div x-show="showModal" class="fixed inset-0 z-10 overflow-y-auto" style="display: none;">
+                            <div
+                                class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                                <!-- Cubierta oscura trasera -->
+                                <div class="fixed inset-0 transition-opacity" aria-hidden="true">
+                                    <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+                                </div>
 
-    <!-- Ventana modal de confirmación -->
-    <div x-show="showModal" class="fixed inset-0 z-10 overflow-y-auto" style="display: none;">
-        <div class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
-            <!-- Cubierta oscura trasera -->
-            <div class="fixed inset-0 transition-opacity" aria-hidden="true">
-                <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
-            </div>
+                                <!-- Centro de la ventana modal -->
+                                <span class="hidden sm:inline-block sm:align-middle sm:h-screen"
+                                    aria-hidden="true">&#8203;</span>
 
-            <!-- Centro de la ventana modal -->
-            <span class="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
-
-            <!-- Contenido de la ventana modal -->
-            <div x-show="showModal" class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
-                style="max-width: 500px;">
-                <div class="bg-white p-5">
-                    <h1 class="text-xl font-bold text-gray-900 mb-4">Eliminar partido</h1>
-                    <p class="text-gray-700">¿Seguro que quieres eliminar este partido? Esta acción no se puede deshacer.</p>
-                </div>
-                <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
-                    <button type="submit"
-                        class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-500 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring focus:border-red-300 sm:ml-3 sm:w-auto sm:text-sm">
-                        Eliminar
-                    </button>
-                    <button type="button" x-on:click="showModal = false"
-                        class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring focus:border-blue-300 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
-                        Cancelar
-                    </button>
-                </div>
-            </div>
-        </div>
-    </div>
-</form>
-
+                                <!-- Contenido de la ventana modal -->
+                                <div x-show="showModal"
+                                    class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+                                    style="max-width: 500px;">
+                                    <div class="bg-white p-5">
+                                        <h1 class="text-xl font-bold text-gray-900 mb-4">Eliminar partido</h1>
+                                        <p class="text-gray-700">¿Seguro que quieres eliminar este partido? Esta acción
+                                            no se puede deshacer.</p>
+                                    </div>
+                                    <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                                        <button type="submit"
+                                            class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-500 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring focus:border-red-300 sm:ml-3 sm:w-auto sm:text-sm">
+                                            Eliminar
+                                        </button>
+                                        <button type="button" x-on:click="showModal = false"
+                                            class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring focus:border-blue-300 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+                                            Cancelar
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                @endif
 
 
 
@@ -189,7 +194,7 @@
                 const weatherInfoDiv = document.getElementById('weather-info');
 
                 if (data.main && data.weather && data.weather.length > 0) {
-                    const temperaturaCelsius =Math.round(data.main.temp);
+                    const temperaturaCelsius = Math.round(data.main.temp);
                     const descripcion = traducirDescripcion(data.weather[0].description);
                     const fecha = fechaPartido; // Utiliza la fecha del partido
                     const icono = obtenerIconoClima(data.weather[0].icon);
